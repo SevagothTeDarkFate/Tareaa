17307f27462d822cc5d7a114f5164a94d325ff36
SevagothTeDarkFate <140095731+SevagothTeDarkFate@users.noreply.github.com>
2024-03-07 22:16:13 -0600

    Add files via upload

---
+++ carpinteria.js
@@ -0,0 +1,196 @@
+class Madera {
+    constructor(nombre, color, textura, tamano, largo, ancho, grosor) {
+        this.nombre = nombre;
+        this.color = color;
+        this.textura = textura;
+        this.tamano = tamano;
+        this.largo = largo;
+        this.ancho = ancho;
+        this.grosor = grosor;
+    }
+    
+    cortar() {
+        console.log(`Cortando la madera ${this.nombre} en piezas de ${this.tamano}`);
+    }
+    
+    pintar() {
+        console.log(`Pintando la madera ${this.nombre} de color ${this.color}`);
+    }
+    
+    lijar() {
+        console.log(`Lijando la madera ${this.nombre} para darle una textura de ${this.textura}`);
+    }
+    
+    barnizar() {
+        console.log(`Barnizando la madera ${this.nombre} para protegerla`);
+    }
+    
+    medir() {
+        console.log(`Midiendo la madera ${this.nombre}: largo ${this.largo} cm, ancho ${this.ancho} cm, grosor ${this.grosor} cm`);
+    }
+    
+    atornillar() {
+        console.log(`Atornillando la madera ${this.nombre}`);
+    }
+}
+
+const madera1 =  new Madera("Pino", "café", "rugosa", "tabla", 120, 30, 2);
+const madera2 =  new Madera("Roble", "caoba", "suave", "plancha", 150, 40, 3);
+const madera3 =  new Madera("Cedro", "rojo", "lisa", "tablon", 180, 50, 4);
+const madera4 =  new Madera("Sándalo", "amarillo", "textura fina", "listón", 90, 25, 2);
+const madera5 =  new Madera("Haya", "blanco", "aspera", "tabla", 110, 35, 2.5);
+const madera6 =  new Madera("Nogal", "marrón oscuro", "rugosa", "plancha", 130, 45, 3.5);
+const madera7 =  new Madera("Teca", "dorado", "rugosa", "tablon", 140, 38, 3);
+const madera8 =  new Madera("Álamo", "blanco", "lisa", "listón", 85, 20, 1.5);
+const madera9 =  new Madera("Bambú", "verde", "fibrosa", "tabla", 95, 22, 2);
+const madera10 = new Madera("Pino", "café oscuro", "rugosa", "plancha", 125, 32, 2.8);
+const madera11 = new Madera("Olmo", "gris", "rugosa", "tablon", 170, 48, 3.8);
+const madera12 = new Madera("Cerezo", "rosa", "lisa", "listón", 80, 18, 1.2);
+const madera13 = new Madera("Abeto", "amarillo claro", "aspera", "tabla", 100, 28, 2.3);
+const madera14 = new Madera("Ébano", "negro", "suave", "plancha", 145, 42, 3.2);
+const madera15 = new Madera("Peral", "blanco amarillento", "fibrosa", "listón", 75, 16, 1.8);
+const madera16 = new Madera("Caoba", "marrón rojizo", "rugosa", "tabla", 115, 33, 2.7);
+const madera17 = new Madera("Castano", "marrón oscuro", "lisa", "plancha", 135, 38, 3.3);
+const madera18 = new Madera("Algarrobo", "marrón claro", "rugosa", "listón", 80, 21, 1.6);
+const madera19 = new Madera("Fresno", "blanco", "aspera", "tablon", 125, 30, 2.5);
+const madera20 = new Madera("Abedul", "blanco", "fibrosa", "tabla", 95, 25, 2);
+
+madera1.medir();
+madera1.cortar();
+madera1.pintar();
+madera1.lijar();
+madera1.barnizar();
+madera1.atornillar();
+
+madera2.medir();
+madera2.cortar();
+madera2.pintar();
+madera2.lijar();
+madera2.barnizar();
+madera2.atornillar();
+
+madera3.medir();
+madera3.cortar();
+madera3.pintar();
+madera3.lijar();
+madera3.barnizar();
+madera3.atornillar();
+
+madera4.medir();
+madera4.cortar();
+madera4.pintar();
+madera4.lijar();
+madera4.barnizar();
+madera4.atornillar();
+
+madera5.medir();
+madera5.cortar();
+madera5.pintar();
+madera5.lijar();
+madera5.barnizar();
+madera5.atornillar();
+
+madera6.medir();
+madera6.cortar();
+madera6.pintar();
+madera6.lijar();
+madera6.barnizar();
+madera6.atornillar();
+
+madera7.medir();
+madera7.cortar();
+madera7.pintar();
+madera7.lijar();
+madera7.barnizar();
+madera7.atornillar();
+
+madera8.medir();
+madera8.cortar();
+madera8.pintar();
+madera8.lijar();
+madera8.barnizar();
+madera8.atornillar();
+
+madera9.medir();
+madera9.cortar();
+madera9.pintar();
+madera9.lijar();
+madera9.barnizar();
+madera9.atornillar();
+
+madera10.medir();
+madera10.cortar();
+madera10.pintar();
+madera10.lijar();
+madera10.barnizar();
+madera10.atornillar();
+
+madera11.medir();
+madera11.cortar();
+madera11.pintar();
+madera11.lijar();
+madera11.barnizar();
+madera11.atornillar();
+
+madera12.medir();
+madera12.cortar();
+madera12.pintar();
+madera12.lijar();
+madera12.barnizar();
+madera12.atornillar();
+
+madera13.medir();
+madera13.cortar();
+madera13.pintar();
+madera13.lijar();
+madera13.barnizar();
+madera13.atornillar();
+
+madera14.medir();
+madera14.cortar();
+madera14.pintar();
+madera14.lijar();
+madera14.barnizar();
+madera14.atornillar();
+
+madera15.medir();
+madera15.cortar();
+madera15.pintar();
+madera15.lijar();
+madera15.barnizar();
+madera15.atornillar();
+
+madera16.medir();
+madera16.cortar();
+madera16.pintar();
+madera16.lijar();
+madera16.barnizar();
+madera16.atornillar();
+
+madera17.medir();
+madera17.cortar();
+madera17.pintar();
+madera17.lijar();
+madera17.barnizar();
+madera17.atornillar();
+
+madera18.medir();
+madera18.cortar();
+madera18.pintar();
+madera18.lijar();
+madera18.barnizar();
+madera18.atornillar();
+
+madera19.medir();
+madera19.cortar();
+madera19.pintar();
+madera19.lijar();
+madera19.barnizar();
+madera19.atornillar();
+
+madera20.medir();
+madera20.cortar();
+madera20.pintar();
+madera20.lijar();
+madera20.barnizar();
+madera20.atornillar();
